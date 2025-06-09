7cd1a14d31f9406a3f422723df9b505a9c73c1d1
reecewadsworth <reecewadsworth@yahoo.co.uk>
2025-06-09 20:32:29 +0100

    Update punctuation_service.py

--- punctuation_service.py
+++ punctuation_service.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from deepmultilingualpunctuation import PunctuationModel
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-model = PunctuationModel(model="oliverguhr/fullstop-punctuation-multilingual-sonar-base")
+model = PunctuationModel()
 
-class TextRequest(BaseModel):
-    text: str
-
-
+# Loader.io verification endpoint (no static mounting needed)
 @app.get("/loaderio-8219b13a341557ee0f2794ceff9f16a2.txt")
 def loaderio_verification():
     return PlainTextResponse("loaderio-8219b13a341557ee0f2794ceff9f16a2")
+
+class TextRequest(BaseModel):
+    text: str
 
 @app.post("/punctuate")
 def punctuate_text(request: TextRequest):
